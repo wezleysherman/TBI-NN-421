591008df8c1c47ed75deff9d283123733f74dbaa
jewlschmidt <julia.schmidt.tkd@gmail.com>
2018-10-22 23:31:17 -0700

    need accurate testing dicom data for testing

--- CTConverter/DICOM_NIFTI.py
+++ CTConverter/DICOM_NIFTI.py
@@ -13,5 +13,19 @@ def convert_dicom_series(input_path):
 
     try:
         dicom2nifti.convert_directory(input_path, output_path)
-    except BaseException:
-        print("something went horribly wrong")
+    except dicom2nifti.exceptions.ConversionError:
+        print("Conversion Failed")
+
+def convert_dicom_single(input_path):
+    folder = os.path.dirname(input_path)
+    output_path = folder + "/nifti/0"
+    if not os.path.exists(output_path):
+        print("here")
+    else:
+        raise FileExistsError("You are trying to convert " +
+            "an already converted scan, available at " +
+            output_path)
+    try:
+        dicom2nifti.dicom_series_to_nifti(input_path, output_path, reorient_nifti=True)
+    except dicom2nifti.exceptions.ConversionError:
+        print("Conversion Failed")
