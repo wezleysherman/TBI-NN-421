0768999ecd1a944ce4f370d39c538ceeba0f514d
jewlschmidt <julia.schmidt.tkd@gmail.com>
2018-10-25 22:55:11 -0700

    refactor so conversion is a part of framework

--- BSSCSFramework/CTConverter/DICOM_operations.py
+++ BSSCSFramework/CTConverter/DICOM_operations.py
@@ -3,7 +3,7 @@ import os
 import sys
 import glob
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from BSSCSFramework.DICOMImporter import DICOMImporter
+from DICOMImporter import DICOMImporter
 
 class DICOM_2_NIFTI:
     #converts a full series of dicom images into a nifti image
@@ -19,31 +19,11 @@ class DICOM_2_NIFTI:
             open(output_path, 'a').close()
 
         dicom_array = DICOMImporter.open_dicom_from_folder(input_path);
-        #else:
-        #    raise FileExistsError("You are trying to convert " +
-        #        "an already converted scan, available at " +
-        #       output_path)
 
         try:
             dicom2nifti.convert_dicom.dicom_array_to_nifti(dicom_array, output_path)
-        #except AttributeError:
         except dicom2nifti.exceptions.ConversionError:
             print("Conversion Failed")
-
-    #converts one dicom image into a nifti image
-    def convert_dicom_single(input_path):
-        folder = os.path.dirname(input_path)
-        output_path = folder + "/nifti/0"
-        if not os.path.exists(output_path):
-            print("here")
-        else:
-            raise FileExistsError("You are trying to convert " +
-                "an already converted scan, available at " +
-                output_path)
-        #try:
-        dicom2nifti.dicom_series_to_nifti(input_path, output_path, reorient_nifti=True)
-        #except dicom2nifti.exceptions.ConversionError:
-        #    print("Conversion Failed")
 
 class decompress_DICOM:
     def decompress_series(input_path):
