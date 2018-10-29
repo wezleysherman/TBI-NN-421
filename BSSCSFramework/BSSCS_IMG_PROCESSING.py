5742b1b7bef5f29559f4ffe1665a72cf73b1ee8b
Wezley Sherman <wezleysherman@yahoo.com>
2018-10-29 11:12:37 -0700

    base img processing class

---
+++ BSSCSFramework/BSSCS_IMG_PROCESSING.py
@@ -0,0 +1,20 @@
+# Authors: Wezley Sherman
+#
+# Reference Attributes: 
+#
+
+import tensorflow as tf
+import numpy as np
+
+class BSSCS_IMG_PROCESSING:
+	def contrast_image(img, contrast_val):
+		'''
+
+		'''
+		return
+
+	def scale_image(img, scale_vector):
+		return
+
+	def crop_image(img, crop_vector):
+		return
