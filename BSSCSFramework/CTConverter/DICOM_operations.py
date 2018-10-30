549ff66b89d4e310a073067d5b99ae41ef05cc4d
jewlschmidt <julia.schmidt.tkd@gmail.com>
2018-10-29 22:59:36 -0700

    added exception handling for DICOM conversion, added Numpy array conversion and output, added related test cases

--- BSSCSFramework/CTConverter/DICOM_operations.py
+++ BSSCSFramework/CTConverter/DICOM_operations.py
@@ -1,4 +1,5 @@
 import dicom2nifti #documentation at https://media.readthedocs.org/pdf/dicom2nifti/latest/dicom2nifti.pdf
+import nibabel #documentation at http://nipy.org/nibabel
 import os
 import sys
 import glob
@@ -6,33 +7,92 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from DICOMImporter import DICOMImporter
 
 class DICOM_2_NIFTI:
-    #converts a full series of dicom images into a nifti image
     def convert_dicom_series(input_path):
+        ''' Converts a raw DICOM file series into a Nifti file.
+            Saves a Nifti file to the file system based on path of DICOM file.
+
+			Input:
+				input_path:
+                    - a directory path to a valid series of DICOM images (more than 1)
+                    - sample directory layout /patient/date/dicom_raw/*.dcm will result in
+                        nifti file located at /patient/date/nifti/date.nii
+			Returns:
+				Numpy array representative of Nifti file
+            Raises:
+                ConversionError if a) files are not decompressed or b) conversion fails
+		'''
         folder = os.path.dirname(input_path)
         output_path = folder + "/nifti/"
         if not os.path.exists(output_path):
             os.mkdir(output_path)
-            output_path += os.path.basename(os.path.dirname(input_path)) + ".nii"
-            open(output_path, 'a').close()
-        else:
-            output_path += os.path.basename(os.path.dirname(input_path)) + ".nii"
-            open(output_path, 'a').close()
 
+        output_path += os.path.basename(os.path.dirname(input_path)) + ".nii"
+        open(output_path, 'a').close()
         dicom_array = DICOMImporter.open_dicom_from_folder(input_path);
 
         try:
             dicom2nifti.convert_dicom.dicom_array_to_nifti(dicom_array, output_path)
+        except NotImplementedError:
+            debug("You tried to convert compressed files.")
+            raise dicom2nifti.exceptions.ConversionError("Cannot convert compressed files.")
+        except OSError:
+            debug("OSError in conversion. Not something to worry about.")
+            open(output_path, 'a').close()
         except dicom2nifti.exceptions.ConversionError:
-            print("Conversion Failed")
+            debug("Conversion Failed")
+            return null;
+
+        return DICOM_2_NIFTI.get_nifti_array(output_path)
+
+    def get_nifti_array(nifti_path):
+        ''' Converts a Nifti file into a Numpy array that represents the 3D image
+
+			Input:
+				input_path:
+                    - a directory path to a valid nifti file
+			Returns:
+				Numpy array representative of Nifti file
+		'''
+        nifti_img = nibabel.load(nifti_path)
+        nifti_data = nifti_img.get_fdata()
+        open(nifti_path, 'a').close()
+        return nifti_data
+
 
 class decompress_DICOM:
     def decompress_series(input_path):
-        if not os.path.exists(os.path.dirname(input_path) + "/dicom_raw"):
-            os.mkdir(os.path.dirname(input_path) + "/dicom_raw")
-        for dicom_path in glob.glob(input_path + '/*.dcm'):
-            os.system("gdcmconv --raw " + dicom_path + " " + os.path.dirname(os.path.dirname(dicom_path)) \
-                        + "/dicom_raw/raw_" + os.path.basename(dicom_path))
+        ''' Decompresses compressed DICOM files and stores them based on path of compressed files
+
+			Input:
+				input_path:
+                    - a directory path to a valid series of DICOM images (more than 1)
+                    - sample directory layout /patient/date/dicom/*.dcm will result in
+                        raw files located at /patient/date/dicom_raw/*.dcm
+            Returns:
+                nothing
+            Raises:
+                FileExistsError if the requested filepath has already been converted
+		'''
+        raw_path = os.path.dirname(input_path) + "/dicom_raw"
+        if not os.path.exists(raw_path):
+            os.mkdir(raw_path)
+            for dicom_path in glob.glob(input_path + '/*.dcm'):
+                os.system("gdcmconv --raw " + dicom_path + " " + os.path.dirname(os.path.dirname(dicom_path)) \
+                            + "/dicom_raw/raw_" + os.path.basename(dicom_path))
+        else:
+            raise FileExistsError("This file has already been decompressed. Raw files available at " + raw_path)
 
     def decompress_single(dicom_path):
+        ''' Decompresses a single DICOM file and stores it based on path of compressed file
+
+			Input:
+				input_path:
+                    - a directory path to a valid DICOM image (more than 1)
+                    - sample directory layout /patient/date/dicom/*.dcm will result in
+                        raw file located at /patient/date/dicom_raw/*.dcm
+		'''
         os.system("gdcmconv --raw " + dicom_path + " " + os.path.dirname(dicom_path) \
                     + "/raw_" + os.path.basename(dicom_path))
+
+def debug(output):
+    print(output);
--- BSSCSFramework/CTConverter/conversion_test_cases.py
+++ BSSCSFramework/CTConverter/conversion_test_cases.py
@@ -1,9 +1,56 @@
 import os
-
+import unittest
+import shutil
+import dicom2nifti
+import numpy
+from retrying import retry
 from DICOM_operations import DICOM_2_NIFTI, decompress_DICOM
 
+dicom_input_path = os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom"
+dicom_output_path = os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom_raw"
+nifti_output_folder = os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/nifti"
+nifti_output_path = os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/nifti/knee.nii"
+
 #decompress images. delete test_images/knee/dicom_raw unless you want to test exception handling
-decompress_DICOM.decompress_series(os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom")
+#decompress_DICOM.decompress_series(os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom")
 #convert images. throws a weird exception for now but it works. use a nifti viewer to view
 #(http://ric.uthscsa.edu/mango/papaya/index.html is a browser one)
-DICOM_2_NIFTI.convert_dicom_series(os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom_raw")
+#DICOM_2_NIFTI.convert_dicom_series(os.path.dirname(os.path.abspath(__file__)) + "/test_images/knee/dicom_raw")
+
+class TestDecompression(unittest.TestCase):
+
+    @retry(stop_max_attempt_number=5, wait_fixed=2000)
+    def setUp(self):
+        if os.path.exists(dicom_output_path):
+            shutil.rmtree(dicom_output_path)
+
+    def test_successful_decompression(self):
+        decompress_DICOM.decompress_series(dicom_input_path)
+        self.assertTrue(os.path.exists(dicom_output_path))
+
+    def test_repeat_decompression(self):
+        decompress_DICOM.decompress_series(dicom_input_path)
+        with self.assertRaisesRegex(FileExistsError, "This file has already been decompressed. Raw files available at") as ex:
+            decompress_DICOM.decompress_series(dicom_input_path)
+
+class TestConversion(unittest.TestCase):
+
+    @retry(stop_max_attempt_number=5, wait_fixed=2000)
+    def setUp(self):
+        if os.path.exists(dicom_output_path):
+            shutil.rmtree(dicom_output_path)
+        if os.path.exists(nifti_output_folder):
+            shutil.rmtree(nifti_output_folder)
+
+    def test_successful_conversion(self):
+        decompress_DICOM.decompress_series(dicom_input_path)
+        nifti_array = DICOM_2_NIFTI.convert_dicom_series(dicom_output_path)
+        self.assertEqual(numpy.core.memmap, type(nifti_array))
+        self.assertTrue(os.path.exists(nifti_output_folder))
+
+    def test_compressed_data(self):
+        with self.assertRaisesRegex(dicom2nifti.exceptions.ConversionError, "Cannot convert compressed files.") as ex:
+            DICOM_2_NIFTI.convert_dicom_series(dicom_input_path)
+
+if __name__ == '__main__':
+    unittest.main()
