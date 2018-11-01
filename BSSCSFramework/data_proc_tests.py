6d20576c7237cc9113d3046def26c0d7d93ab133
Wezley Sherman <wezleysherman@yahoo.com>
2018-10-31 17:27:17 -0700

    fixed git issues

---
+++ BSSCSFramework/data_proc_tests.py
@@ -0,0 +1,20 @@
+''' Authors: Wezley Sherman
+	Unit Tests for the data processing framework implementation
+	Unit Test Reference: https://docs.python.org/2/library/unittest.html
+'''
+from DICOMImporter import DICOMImporter
+from BSSCS_IMG_PROCESSING import BSSCS_IMG_PROCESSING
+import unittest
+
+class DataProcessingTests(unittest.TestCase):
+	def test_dicom_importer():
+		return
+	def test_contrast_adjust():
+		return
+	def test_scale_adjust():
+		return
+	def test_crop_adjust():
+		return
+
+if __name__ == '__main__':
+	unittest.main()
