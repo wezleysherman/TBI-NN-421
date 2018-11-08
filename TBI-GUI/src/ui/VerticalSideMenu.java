76d707f926c24106c36678120f91ce6f25745984
CanyonSchubert <canyonschubert72@gmail.com>
2018-11-07 18:56:23 -0700

    Home Button Warning
    home button displays warning message when pressed

--- TBI-GUI/src/ui/VerticalSideMenu.java
+++ TBI-GUI/src/ui/VerticalSideMenu.java
@@ -3,13 +3,18 @@ package ui;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.ButtonType;
 import java.util.EmptyStackException;
 
 public class VerticalSideMenu {
@@ -23,6 +28,20 @@ public class VerticalSideMenu {
 		Button homeBtn = new Button();
 		Label pageName = new Label();
 		Pane colorPane = new Pane();
+		
+		// Home Button Dialog SetUp TODO: Find a way to center the yes/no buttons
+		Dialog<ButtonType> homeWarning = new Dialog<ButtonType>();
+		homeWarning.setTitle("Warning!");
+		homeWarning.setHeaderText("Returning to the home page will cause you to lose all unsaved information!");
+		homeWarning.setResizable(false);
+		GridPane hwPane = new GridPane();
+		Label hwLabel = new Label("Continue?");
+		hwPane.add(hwLabel, 1, 0);
+		hwPane.setAlignment(Pos.CENTER);
+		ButtonType hwConfirm = new ButtonType("Yes", ButtonData.YES);
+		ButtonType hwDecline = new ButtonType("No", ButtonData.NO);
+		homeWarning.getDialogPane().setContent(hwPane);
+		homeWarning.getDialogPane().getButtonTypes().addAll(hwConfirm, hwDecline);	
 		
 		//Button Setup
 		backBtn.setText("BACK");
@@ -49,8 +68,13 @@ public class VerticalSideMenu {
 			
 			@Override
 			public void handle(ActionEvent arg0) {
-				manager.sceneStack.clear();
-				manager.paintScene("landing");
+				
+				homeWarning.showAndWait();
+				
+				if (homeWarning.getResult().getButtonData().equals(ButtonData.YES)) {
+					manager.sceneStack.clear();
+					manager.paintScene("landing");
+				}
 			}
 			
 		});
