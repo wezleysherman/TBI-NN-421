265950ba2d8f027389af42fc5240455cc1f8bdeb
CanyonSchubert <canyonschubert72@gmail.com>
2018-11-12 10:07:42 -0700

    started side menu for viewScan page
    and created a scan class that I'm sending to a new branch to work on

---
+++ TBI-GUI/src/ui/Scan.java
@@ -0,0 +1,47 @@
+package ui;
+
+import java.util.Date;
+import java.io.File;
+
+public class Scan {
+
+	private Date dateOfScan;
+	private File scan;
+	private String notes;
+	
+	public Scan(Date dOS, File inScan) {
+		this.setDateOfScan(dOS);
+		this.setScan(inScan);
+	}
+	
+	public Scan(Date dOS, File inScan, String inNotes) {
+		this.setDateOfScan(dOS);
+		this.setScan(inScan);
+		this.setNotes(inNotes);
+	}
+
+	public Date getDateOfScan() {
+		return dateOfScan;
+	}
+
+	public void setDateOfScan(Date dateOfScan) {
+		this.dateOfScan = dateOfScan;
+	}
+
+	public File getScan() {
+		return scan;
+	}
+
+	public void setScan(File scan) {
+		this.scan = scan;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+}
