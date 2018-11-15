ed3dc0ff242d8d69a063fe885a74fc87e7da4771
wezleysherman <wezleysherman@yahoo.com>
2018-11-15 11:26:33 -0700

    Finished test cases

--- BSSCSFramework/classifier_tests.py
+++ BSSCSFramework/classifier_tests.py
@@ -18,7 +18,16 @@ class ClassifierNetworkTests(unittest.TestCase):
 			preconditions: No layer has been created
 			postconditions: layer with default placeholder has been created
 		'''
-		return
+		bsscs = BSSCS_CLASSIFIER(
+			l2_reg=tf.contrib.layers.l2_regularizer(scale=0.001),
+			learning_rate=0.001,
+			steps=250, # Arbitrary
+			batch=250, # Arbitrary
+		)
+		self.assertTrue(bsscs != None)
+		# Tests that a single layer is created and returned
+		single_layer = bsscs.create_layer(512, activation=tf.nn.relu)
+		self.assertTrue(single_layer != None)
 
 	def test_loss_creation(self):
 		''' Tests the creation of a loss function
@@ -26,7 +35,23 @@ class ClassifierNetworkTests(unittest.TestCase):
 			preconditions: no loss function has been created
 			postconditions: A loss function has been created and exists
 		'''
-		return
+		bsscs = BSSCS_CLASSIFIER(
+			l2_reg=tf.contrib.layers.l2_regularizer(scale=0.001),
+			learning_rate=0.001,
+			steps=250, # Arbitrary
+			batch=250, # Arbitrary
+		)
+		self.assertTrue(bsscs != None)
+
+		# Tests that a loss function is created and assigned to the bsscs object
+		input_ph = tf.placeholder(tf.float32, shape=[None, 512, 512]) # 512x512 images
+		labels_ph = tf.placeholder(tf.float32, shape=[None, 2]) # Testing against 2 labels
+		loss_function = bsscs.create_loss_function(input=input_ph, labels=labels_ph)
+		self.assertTrue(loss_function != None)
+
+		# Test that the loss function was set in the object
+		loss_function_var = bsscs.get_loss()
+		self.assertTrue(loss_function_var == loss_function)
 
 	def test_optimizer_creation(self):
 		''' Tests the creation of an optimizer for the tests
@@ -34,7 +59,21 @@ class ClassifierNetworkTests(unittest.TestCase):
 			preconditions: an optmizer hasn't been created
 			postconditions: an optimizer has been created
 		'''
-		return
+		bsscs = BSSCS_CLASSIFIER(
+			l2_reg=tf.contrib.layers.l2_regularizer(scale=0.001),
+			learning_rate=0.001,
+			steps=250, # Arbitrary
+			batch=250, # Arbitrary
+		)
+		self.assertTrue(bsscs != None)
+
+		# Tests that an optimizer function is created and assigned to the bsscs object
+		optimizer = bsscs.create_optimizer()
+		self.assertTrue(optimizer != None)
+
+		optimizer_var = bsscs.get_optimizer()
+		self.assertTrue(optimizer == optimizer_var)
+
 
 	def test_classifier_creation(self):
 		''' Tests the creation of a full FC graph
@@ -42,7 +81,16 @@ class ClassifierNetworkTests(unittest.TestCase):
 			preconditions: a FC graph hasn't been created
 			postconditions: a FC graph has been created covering all functions
 		'''
-		return
+		bsscs = BSSCS_CLASSIFIER(
+			l2_reg=tf.contrib.layers.l2_regularizer(scale=0.001),
+			learning_rate=0.001,
+			steps=250, # Arbitrary
+			batch=250, # Arbitrary
+		)
+		self.assertTrue(bsscs != None)
+
+		classifier = bsscs.create_classifier([125, 12])
+		self.assertTrue(classifier != None)
 
 if __name__ == '__main__':
 	unittest.main()
