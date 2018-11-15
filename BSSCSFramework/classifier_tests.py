8a4694ae3446097efe07dae11cb69c63c3fe1b3e
Wezley Sherman <wezleysherman@yahoo.com>
2018-11-14 17:29:20 -0700

    Test cases laid out completely

--- BSSCSFramework/classifier_tests.py
+++ BSSCSFramework/classifier_tests.py
@@ -11,4 +11,38 @@ import numpy as np
 import unittest
 import tensorflow as tf
 
-class ClassifierNetworkTests(unittest.TestCase);
+class ClassifierNetworkTests(unittest.TestCase):
+	def test_layer_creation(self):
+		''' Tests the creation of a default layer
+
+			preconditions: No layer has been created
+			postconditions: layer with default placeholder has been created
+		'''
+		return
+
+	def test_loss_creation(self):
+		''' Tests the creation of a loss function
+
+			preconditions: no loss function has been created
+			postconditions: A loss function has been created and exists
+		'''
+		return
+
+	def test_optimizer_creation(self):
+		''' Tests the creation of an optimizer for the tests
+
+			preconditions: an optmizer hasn't been created
+			postconditions: an optimizer has been created
+		'''
+		return
+
+	def test_classifier_creation(self):
+		''' Tests the creation of a full FC graph
+
+			preconditions: a FC graph hasn't been created
+			postconditions: a FC graph has been created covering all functions
+		'''
+		return
+
+if __name__ == '__main__':
+	unittest.main()
