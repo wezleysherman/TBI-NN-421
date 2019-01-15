27a98ece65024bbc207cb314df89831b5f1a8cd5
CanyonSchubert <canyonschubert72@gmail.com>
2019-01-15 11:36:49 -0700

    Filepicker for .csv files
    still learning but we're getting there and we have a functional filepicker now

--- BSSCSFramework/NN-GUI/App.py
+++ BSSCSFramework/NN-GUI/App.py
@@ -1,23 +1,35 @@
-#using the tutorial here: https://build-system.fman.io/pyqt5-tutorial INFO ON TURNING INTO STANDALONE .EXE AT THE END
+#using the tutorials here: https://build-system.fman.io/pyqt5-tutorial INFO ON TURNING INTO STANDALONE .EXE AT THE END
+#(FILE DIALOGS / BASIC GUI SETUP) http://zetcode.com/gui/pyqt5/dialogs/
 #CURRENTLY NOT USED (FILE DIALOGS) https://pythonspot.com/pyqt5-file-dialog/
 #CURRENTLY NOT USED (STYLES) http://doc.qt.io/qt-5/qtwidgets-index.html#styles
 
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QFileDialog, QPushButton, QMessageBox
-app = QApplication([])
-app.setStyle("Fusion")
-window = QWidget()
-layout = QVBoxLayout()
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QFileDialog, QPushButton, QMessageBox, QLineEdit
+import sys
 
-csv_button = QPushButton("Choose a .CSV File")
-def csv_clicked():
-    csv_filepicker = QFileDialog()
-    """alert = QMessageBox()
-    alert.setText("Insert .CSV File Picker Here!")
-    alert.exec_()"""
-csv_button.clicked.connect(csv_clicked)
+class NNGUI(QWidget):
 
-layout.addWidget(csv_button)
-window.setLayout(layout)
-window.show()
-app.exec_()
+    def __init__(self):
+        super().__init__()
+        self.initNNGUI()
 
+    def initNNGUI(self):
+        layout = QVBoxLayout()
+        csv_filepath = QLineEdit(".CSV Filename Here")
+        csv_filepath.setReadOnly(True)
+        csv_button = QPushButton("Choose a .CSV File")
+        def csv_clicked():
+            csv_filepicker = QFileDialog.getOpenFileName(self, "CSV File Picker", "/home", "CSV (*.csv)")
+            csv_filepath.setText(csv_filepicker[0])
+
+        csv_button.clicked.connect(csv_clicked)
+        layout.addWidget(csv_filepath)
+        layout.addWidget(csv_button)
+        self.setGeometry(300, 300, 350, 300)
+        self.setLayout(layout)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    nngui = NNGUI()
+    sys.exit(app.exec_())
