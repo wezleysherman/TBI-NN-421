0ccf1ef1cc14f377ac4e4912dfe17329d834a92f
Wezley Sherman <wezleysherman@yahoo.com>
2019-01-23 14:46:40 -0700

    US278-Task306-Task304-Task305

--- BSSCSFramework/BSSCS_DATA_PROCESSOR.py
+++ BSSCSFramework/BSSCS_DATA_PROCESSOR.py
@@ -6,17 +6,26 @@
 #
 # This class is a part of the BSSCS Net Framework to import DICOM images and convert them from DICOM to NIFTI
 from DICOMImporter import DICOMImporter
-from CTConverter.DICOM_operations import DICOM_2_NIFTI, decompress_DICOM, prepare_scans
+from CTConverter.DICOM_operations import DICOM_2_NIFTI, prepare_scans
 import pandas as pd
 import numpy as np
-
+import pydicom
 
 class BSSCS_DATA_PROCESSOR:
-	def __init__(self, path_to_data):
+	def __init__(self, dicom_path, nifti_path, output_path):
 		self.path = path_to_data
 		self.dicoms_orig = []
 		self.dicoms_norm = []
 		self.nifti = []
+		self.dicom_path = dicom_path
+		self.nifti_path = nifti_path
+		self.dicom_output = output_path
+		self.total_dicoms = 0
+		self.total_nifti = 0
+		self.import_dicom_data()
+		self.normalize_dicom_data()
+		self.convert_dicom_to_nifti()
+
 
 	def import_dicom_data(self):
 		''' Handles mass importing all DICOMs from a folder
@@ -27,7 +36,9 @@ class BSSCS_DATA_PROCESSOR:
 			Returns: 
 				- None	
 		'''
-		return None # lol
+		self.dicoms_orig = DICOMImporter.open_dicom_from_folder(self.dicom_path)
+		self.total_dicoms = len(self.dicoms_orig)
+
 
 	def convert_dicom_to_nifti(self):
 		''' Handles converting normalized dicoms to nifti
@@ -38,7 +49,10 @@ class BSSCS_DATA_PROCESSOR:
 			Returns: 
 				- None	
 		'''
-		return None
+		dicom_path = prepare_scans.convert(self.output_path)
+		self.nifti = DICOM_2_NIFTI.get_nifti_array(dicom_path)
+		self.total_nifti = len(self.nifti)
+		
 
 	def normalize_dicom_data(self):
 		''' Handles normalizing imported dicom data
@@ -49,7 +63,10 @@ class BSSCS_DATA_PROCESSOR:
 			Returns: 
 				- None	
 		'''
-		return None
+		self.dicom_norm = DICOMImporter.deidentify_dicom_images(self.dicoms_orig)
+		for i, dicom in enumerate(self.dicom_norm):
+			pydicom.write_file(self.output_path + '/anon-' + str(i) + '.dcm')
+
 
 	def fetch_norm_dicoms(self):
 		''' Handles returning the normalized dicom array
