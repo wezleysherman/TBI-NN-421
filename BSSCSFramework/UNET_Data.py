096f199e97bd01666db1be25ad97f3a106638cdb
Wezley Sherman <wezleysherman@yahoo.com>
2019-01-24 20:41:55 -0700

    US278-Task309-Task308-Import-Images-From-CSV

--- BSSCSFramework/UNET_Data.py
+++ BSSCSFramework/UNET_Data.py
@@ -9,6 +9,8 @@
 # BSSCS Docs Importer location: BSSCS_DOCS/dicom.html
 from DICOMImporter import DICOMImporter
 import pandas as pd
+import numpy as np
+from PIL import Image
 
 class UNET_DATA:
 	def __init__(self, labels_arr=None, image_arr=None):
@@ -63,7 +65,49 @@ class UNET_DATA:
 		images = list(csv_dataframe['file_name'])
 		labels = list(csv_dataframe['has_tbi'])
 		return images, labels
+
+	def open_csv(self, path):
+		''' Handles opening a labels CSV for the test set and returning the datframe
+
+			Input:
+				- path: String -- path to CSV
+
+			Returns:
+				- csv_dataframe: pandas dataframe for labels
+
+		'''
+		csv_dataframe = pd.read_csv(path)
+		return csv_dataframe
+
+	def fetch_images_with_csv(self, path, dataframe):
+		''' Handles fetching images from a filepath and constructs a dictionary with their labels
+
+			Input:
+				- path: String -- path to data folder
+				- dataframe: pandas dataframe
+
+			Returns:
+				- Dictionary of data structured as:
+				{
+					image_name : {
+						image_arr: [2D pixel array],
+						labels: [labels array]
+					}
+				}
+		'''
+		data_dictionary = {}
+		for row in dataframe.iterrows():
+			data_dictionary[row[1][0]] = {}
+			image_path = path +'/' + row[1][0] + '_blue.png'
+			image = list(Image.open(image_path).getdata())
+			data_dictionary[row[1][0]]['image_arr'] = image
+			data_dictionary[row[1][0]]['labels'] = row[1][1]
+			print(data_dictionary)
+		return data_dictionary
+			
 		
-#unet = UNET_DATA()
+unet = UNET_DATA()
 #print(unet.import_labels_from_csv("test_csv.csv")[1])
 #print(unet.fetch_data("test_csv.csv"))
+data_frame = unet.open_csv('Data_right/train.csv')
+print(unet.fetch_images_with_csv('Data_right/Train/train', data_frame))
