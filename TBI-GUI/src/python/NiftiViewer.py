a4fbf70c19030f48129dfe7cdbc6fec9e5de70c5
wabaker1 <wabaker@asu.edu>
2019-02-25 12:48:41 -0700

    Add loading screen and opening of niftiviewer to the normal flow of the program

--- TBI-GUI/src/python/NiftiViewer.py
+++ TBI-GUI/src/python/NiftiViewer.py
@@ -22,13 +22,15 @@ def update(val):
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 8080))
+s.close()
 
 file = sys.argv[1]
 image = nibabel.load(file)
+image_size = image.shape[2] - 1
 data_arr = image.get_data().T
 global im
 im = axes.imshow(data_arr[0])
 sliderAxe = plt.axes([0.15, 0.02, 0.65, 0.03])
-slice = Slider(sliderAxe, "Slice:", 0, 21, 0, '%1.2f', valstep=1)
+slice = Slider(sliderAxe, "Slice:", 0, image_size, 0, '%1.2f', valstep=1)
 slice.on_changed(update)
 plt.show()
