9708dd7243592327600f0a5cfab6c871f798108f
wabaker1 <wabaker@asu.edu>
2019-03-14 12:05:17 -0700

    Add ability for niftiviewer to open image files such as png or jpeg

--- TBI-GUI/src/python/NiftiViewer.py
+++ TBI-GUI/src/python/NiftiViewer.py
@@ -2,6 +2,7 @@ import nibabel
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, ion, show
 from matplotlib.widgets import Slider
+import matplotlib.image as img
 import os
 import sys
 import socket
@@ -25,12 +26,18 @@ s.connect(("127.0.0.1", 8080))
 s.close()
 
 file = sys.argv[1]
-image = nibabel.load(file)
-image_size = image.shape[2] - 1
-data_arr = image.get_data().T
-global im
-im = axes.imshow(data_arr[0])
-sliderAxe = plt.axes([0.15, 0.02, 0.65, 0.03])
-slice = Slider(sliderAxe, "Slice:", 0, image_size, 0, '%1.2f', valstep=1)
-slice.on_changed(update)
-plt.show()
+ext = os.path.splitext(file)[1]
+if(ext == ".nii" or ext == ".dicom"):
+    image = nibabel.load(file)
+    image_size = image.shape[2] - 1
+    data_arr = image.get_data().T
+    global im
+    im = axes.imshow(data_arr[0])
+    sliderAxe = plt.axes([0.15, 0.02, 0.65, 0.03])
+    slice = Slider(sliderAxe, "Slice:", 0, image_size, 0, '%1.2f', valstep=1)
+    slice.on_changed(update)
+    plt.show()
+else:
+    image = img.imread(file)
+    imgPlot = plt.imshow(image)
+    plt.show()
